ef6fd7a142b009f741a4b0557d7c2c5640606023
Eric Cui <crqhit@163.com>
2020-09-02 12:48:40 +0800

    关闭文件大小差值判断

--- ImageProcess/Image.cpp
+++ ImageProcess/Image.cpp
@@ -141,8 +141,7 @@ void Image::nameContinuous(QString &name)
 void Image::sortie(ImageInfo info)
 {
 	//更换架次
-	if (fabs(info.timestamp - m_info.timestamp) > m_fightGap || 
-		fabs(info.size - m_info.size) > m_sizeDiff)
+	if (fabs(info.timestamp - m_info.timestamp) > m_fightGap)  // || fabs(info.size - m_info.size) > m_sizeDiff
 	{
 		Log::INFO(QStringLiteral("时间间隔： %1， 照片大小差： %2").arg(m_fightGap).arg(m_sizeDiff));
 
